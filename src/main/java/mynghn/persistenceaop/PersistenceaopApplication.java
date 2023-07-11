2fb05b94b4b44f859453464f7193fadea3703947
Myounghoon Lee <l.mynghn@gmail.com>
2023-07-11 18:33:30 +0900

    Initial commit

---
+++ src/main/java/mynghn/persistenceaop/PersistenceaopApplication.java
@@ -0,0 +1,13 @@
+package mynghn.persistenceaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceaopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceaopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mynghn/persistenceaop/PersistenceaopApplicationTests.java
@@ -0,0 +1,13 @@
+package mynghn.persistenceaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceaopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
