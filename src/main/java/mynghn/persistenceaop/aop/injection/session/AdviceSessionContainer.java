c10f2541d3d2d8d2b0cf810669b23fa8d38fc626
Myounghoon Lee <l.mynghn@gmail.com>
2023-07-30 01:16:19 +0900

    chore: remove unused logger

--- src/main/java/mynghn/persistenceaop/aop/injection/session/AdviceSessionContainer.java
+++ src/main/java/mynghn/persistenceaop/aop/injection/session/AdviceSessionContainer.java
@@ -1,8 +1,5 @@
 package mynghn.persistenceaop.aop.injection.session;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public final class AdviceSessionContainer extends ThreadLocal<AdviceSession> {
 
     private static final AdviceSessionContainer instance = new AdviceSessionContainer();
