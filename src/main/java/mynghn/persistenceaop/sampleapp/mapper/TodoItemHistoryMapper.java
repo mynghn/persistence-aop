fbec07cb6d34508276b768c7293e936fe71c482f
Myounghoon Lee <l.mynghn@gmail.com>
2023-09-19 00:46:49 +0900

    chore: Remove unused codes

--- src/main/java/mynghn/persistenceaop/sampleapp/mapper/TodoItemHistoryMapper.java
+++
@@ -1,9 +0,0 @@
-package mynghn.persistenceaop.sampleapp.mapper;
-
-import mynghn.persistenceaop.sampleapp.entity.TodoItem;
-import mynghn.persistenceaop.mapper.base.HistoryMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TodoItemHistoryMapper extends HistoryMapper<TodoItem> {
-}
--- src/test/resources/test-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO PUBLIC.todo_list(id, title) VALUES ('230712-000', 'Test Todo List 1');
-INSERT INTO PUBLIC.todo_list(id, title) VALUES ('230713-000', 'Test Todo List 2');
-
